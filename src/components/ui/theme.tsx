861d60ceefff2d0dda6ec122e7efaa00891012f5
Samir Khanal <gdssamir@gmail.com>
2025-03-08 21:17:51 +0545

    removed emotion/react

--- src/components/ui/theme.tsx
+++ src/components/ui/theme.tsx
@@ -1,11 +1,7 @@
 "use client";
-
-/** @jsxImportSource @emotion/react */
-
 import * as React from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
